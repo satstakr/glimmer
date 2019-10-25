c5f38cf89db86c32cc495462e12879fade810172
Joe-Tomlinson <jpt.5656@gmail.com>
2019-10-25 10:32:11 -0500

    Added .json file

--- glimmerchain/block.test.js
+++ glimmerchain/block.test.js
@@ -1,7 +1,14 @@
 describe('Block', () => {
-  const timestamp;
-  const lastHash;
-  const hash;
-  const data;
-  const block;
-})
+  const timestamp = 'a date';
+  const lastHash = 'glim-hash';
+  const hash = 'glimmer-hash';
+  const data = ['blockchain', 'data'];
+  const block = new Block([ timestamp, lastHash, hash, data, ]);
+
+  it('has a timestamp, lastHash, hash, and a data property', () => {
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.lastHash).toEqual(lastHash);
+    expect(block.hash).toEqual(hash);
+    expect(block.data).toEqual(data);
+  });
+});
